a92c3bf3fb4fd67d2895a822ac86edfdef8954fd
Elizandra Monteiro <monteiroelizandra2017@gmail.com>
2023-11-15 14:47:37 -0300

    API

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,11 @@
+//rotas de usuário
+const { Router } = require("express");
+const TagsController = require("../controllers/TagsController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const tagsRoutes = Router();
+
+const tagsController = new TagsController();
+
+tagsRoutes.get("/", ensureAuthenticated, tagsController.index);
+
+module.exports = tagsRoutes;
