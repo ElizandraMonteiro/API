99f84ff229837b4a3ed3da5ff4deaa3e8836e648
Elizandra Monteiro <monteiroelizandra2017@gmail.com>
2023-11-16 17:43:17 -0300

    Alteration

--- src/server.js
+++ src/server.js
@@ -12,7 +12,14 @@ const express = require('express');
 const routes = require("./routes")
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
