9370cf05ad6dc20e42a438a6f7c918c1f6d37cfb
Elizandra Monteiro <monteiroelizandra2017@gmail.com>
2023-11-16 20:32:19 -0300

    fix cors app.use

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -3,6 +3,8 @@ const sqlite = require("sqlite");
 const path = require("path");
 
 
+
+
 async function sqliteConnection() {
     const database = await sqlite.open({
         filename: path.resolve(__dirname, "..", "database.db"),
